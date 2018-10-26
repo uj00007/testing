062aa6add93c172e972ccf5bad178a1a52a98181
ujjwalgoyal007 <ujjwal.g@hashedin.com>
2018-10-26 14:43:07 +0530

    added App.js

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from './styles';
+
+const App = () => (
+  <View style={styles.container}>
+    <Text style={styles.welcome}>Welcome to React Native!</Text>
+    <Text style={styles.instructions}>To get started, edit App.js</Text>
+  </View>
+);
+
+export default App;
