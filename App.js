ccdf456b91642f8302782e35ccd3272c8d7ac87d
ujjwalgoyal007 <ujjwal.g@hashedin.com>
2018-11-16 11:31:59 +0530

    made some initial commit

--- App.js
+++ App.js
@@ -8,12 +8,21 @@
 
 import React from 'react';
 import { Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 import styles from './styles';
 
 const App = () => (
   <View style={styles.container}>
     <Text style={styles.welcome}>Welcome to React Native!</Text>
     <Text style={styles.instructions}>To get started, edit App.js</Text>
+    <MapView
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      }}
+    />
   </View>
 );
 
